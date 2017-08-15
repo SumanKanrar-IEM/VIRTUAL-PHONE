a5532ff74e7a25aecf4d38121e0776960ddb4830
SumanKanrar-IEM <sumankanrar25@gmail.com>
2017-08-16 02:17:42 +0530

    Subsequently, changed the project_ui.py file

--- project_ui.py
+++ project_ui.py
@@ -2,59 +2,46 @@
 
 # Form implementation generated from reading ui file 'project.ui'
 #
-# Created by: PyQt4 UI code generator 4.11.4
+# Created by: PyQt5 UI code generator 5.9
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
-        Dialog.setObjectName(_fromUtf8("Dialog"))
-        Dialog.resize(597, 393)
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(363, 440)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(_fromUtf8("../Pictures/Feedback/{4AE122AA-3DEE-4204-9588-9FCAC4BAEA3D}/Capture001.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap("../Pictures/Feedback/{4AE122AA-3DEE-4204-9588-9FCAC4BAEA3D}/Capture001.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         Dialog.setWindowIcon(icon)
         Dialog.setSizeGripEnabled(False)
-        self.label = QtGui.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(10, 20, 271, 91))
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(20, 30, 341, 91))
         font = QtGui.QFont()
-        font.setFamily(_fromUtf8("Comic Sans MS"))
+        font.setFamily("Comic Sans MS")
         font.setPointSize(28)
         font.setBold(True)
         font.setItalic(True)
         font.setWeight(75)
         self.label.setFont(font)
-        self.label.setObjectName(_fromUtf8("label"))
-        self.label_2 = QtGui.QLabel(Dialog)
-        self.label_2.setGeometry(QtCore.QRect(20, 110, 111, 41))
+        self.label.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(110, 130, 141, 41))
         font = QtGui.QFont()
-        font.setFamily(_fromUtf8("Arial Narrow"))
+        font.setFamily("Arial Narrow")
         font.setPointSize(14)
         font.setBold(False)
         font.setWeight(50)
         self.label_2.setFont(font)
-        self.label_2.setObjectName(_fromUtf8("label_2"))
-        self.frame = QtGui.QFrame(Dialog)
-        self.frame.setGeometry(QtCore.QRect(10, 150, 251, 181))
-        self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
-        self.frame.setFrameShadow(QtGui.QFrame.Raised)
-        self.frame.setObjectName(_fromUtf8("frame"))
-        self.pushButton = QtGui.QPushButton(self.frame)
+        self.label_2.setObjectName("label_2")
+        self.frame = QtWidgets.QFrame(Dialog)
+        self.frame.setGeometry(QtCore.QRect(50, 180, 231, 211))
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.pushButton = QtWidgets.QPushButton(self.frame)
         self.pushButton.setGeometry(QtCore.QRect(30, 60, 75, 31))
         font = QtGui.QFont()
         font.setPointSize(10)
@@ -63,8 +50,8 @@ class Ui_Dialog(object):
         font.setWeight(75)
         self.pushButton.setFont(font)
         self.pushButton.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
-        self.pushButton.setObjectName(_fromUtf8("pushButton"))
-        self.pushButton_2 = QtGui.QPushButton(self.frame)
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.frame)
         self.pushButton_2.setGeometry(QtCore.QRect(120, 60, 75, 31))
         font = QtGui.QFont()
         font.setPointSize(10)
@@ -72,8 +59,8 @@ class Ui_Dialog(object):
         font.setWeight(75)
         self.pushButton_2.setFont(font)
         self.pushButton_2.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
-        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
-        self.pushButton_3 = QtGui.QPushButton(self.frame)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(self.frame)
         self.pushButton_3.setGeometry(QtCore.QRect(80, 110, 75, 31))
         font = QtGui.QFont()
         font.setPointSize(10)
@@ -81,35 +68,26 @@ class Ui_Dialog(object):
         font.setWeight(75)
         self.pushButton_3.setFont(font)
         self.pushButton_3.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
-        self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
-        self.comboBox = QtGui.QComboBox(self.frame)
-        self.comboBox.setGeometry(QtCore.QRect(20, 10, 171, 22))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.comboBox = QtWidgets.QComboBox(self.frame)
+        self.comboBox.setGeometry(QtCore.QRect(30, 10, 171, 22))
         self.comboBox.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
         self.comboBox.setAcceptDrops(False)
-        self.comboBox.setObjectName(_fromUtf8("comboBox"))
-        self.commandLinkButton = QtGui.QCommandLinkButton(Dialog)
-        self.commandLinkButton.setGeometry(QtCore.QRect(360, 180, 185, 41))
-        self.commandLinkButton.setObjectName(_fromUtf8("commandLinkButton"))
-        self.label_3 = QtGui.QLabel(Dialog)
-        self.label_3.setGeometry(QtCore.QRect(360, 120, 201, 71))
-        font = QtGui.QFont()
-        font.setFamily(_fromUtf8("Arial Black"))
-        font.setPointSize(12)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label_3.setFont(font)
-        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.comboBox.setObjectName("comboBox")
+        self.commandLinkButton = QtWidgets.QCommandLinkButton(self.frame)
+        self.commandLinkButton.setGeometry(QtCore.QRect(30, 150, 161, 41))
+        self.commandLinkButton.setObjectName("commandLinkButton")
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog", None))
-        self.label.setText(_translate("Dialog", "Address Book", None))
-        self.label_2.setText(_translate("Dialog", "Select Contact:", None))
-        self.pushButton.setText(_translate("Dialog", "Call", None))
-        self.pushButton_2.setText(_translate("Dialog", "Sms", None))
-        self.pushButton_3.setText(_translate("Dialog", "E-mail", None))
-        self.commandLinkButton.setText(_translate("Dialog", "Add Contact", None))
-        self.label_3.setText(_translate("Dialog", "Not Registered Yet??", None))
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "Address Book"))
+        self.label_2.setText(_translate("Dialog", "Select Contact:"))
+        self.pushButton.setText(_translate("Dialog", "Call"))
+        self.pushButton_2.setText(_translate("Dialog", "Sms"))
+        self.pushButton_3.setText(_translate("Dialog", "E-mail"))
+        self.commandLinkButton.setText(_translate("Dialog", "Add Contact"))
 
