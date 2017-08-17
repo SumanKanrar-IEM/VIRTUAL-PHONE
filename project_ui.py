a7ba4c55be2973b12bd6a919ad99eb902943bba8
SumanKanrar-IEM <SumanKanrar-IEM@users.noreply.github.com>
2017-08-17 16:03:09 +0530

    Made changes to the project_ui.py file to make it executable

--- project_ui.py
+++ project_ui.py
@@ -92,3 +92,12 @@ class Ui_Dialog(object):
         self.commandLinkButton.setText(_translate("Dialog", "Add Contact"))
 
 
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
+
