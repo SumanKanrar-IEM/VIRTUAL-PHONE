0aa451c9d4289758566d89ca389fdc266c31a73d
SumanKanrar-IEM <SumanKanrar-IEM@users.noreply.github.com>
2017-09-21 22:19:49 +0530

    The homescreen .py filed added

---
+++ homescreen/main.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'main.ui'
+#
+# Created by: PyQt5 UI code generator 5.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(336, 475)
+        Dialog.setStyleSheet("background-image: url(:/new/light-backgrounds-related-keywords-suggestions-light-backgrounds-lighting-background-l-76edaab432453c3b.jpg);")
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(60, 80, 81, 71))
+        self.pushButton.setStyleSheet("image: url(:/new/256-256-64273d52c282e3b26d2d0968d08b9d8d.png);\n"
+"")
+        self.pushButton.setText("")
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_2.setGeometry(QtCore.QRect(200, 80, 81, 71))
+        self.pushButton_2.setStyleSheet("image: url(:/new/Circle-icons-contacts.svg.png);")
+        self.pushButton_2.setText("")
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_3.setGeometry(QtCore.QRect(60, 190, 81, 71))
+        self.pushButton_3.setStyleSheet("image: url(:/new/browser-icon.png);")
+        self.pushButton_3.setText("")
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_4 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_4.setGeometry(QtCore.QRect(200, 320, 81, 71))
+        self.pushButton_4.setStyleSheet("image: url(:/new/cloud.png);")
+        self.pushButton_4.setText("")
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_5 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_5.setGeometry(QtCore.QRect(60, 320, 81, 71))
+        self.pushButton_5.setStyleSheet("image: url(:/new/note-icon-flat_20610.png);")
+        self.pushButton_5.setText("")
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.pushButton_6 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_6.setGeometry(QtCore.QRect(200, 190, 81, 71))
+        self.pushButton_6.setStyleSheet("image: url(:/new/calculator-flat.png);")
+        self.pushButton_6.setText("")
+        self.pushButton_6.setObjectName("pushButton_6")
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
+
