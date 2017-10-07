5c59ee871bf3ad442d7928a0cb1641e581a946d9
SumanKanrar-IEM <SumanKanrar-IEM@users.noreply.github.com>
2017-10-08 02:32:56 +0530

    Icons resize done and background changed

--- homescreen/homescreen.py
+++ homescreen/homescreen.py
@@ -7,18 +7,20 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QSize
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(431, 668)
-        Dialog.setStyleSheet("background-image: url(background.jpg);")
+        Dialog.setStyleSheet("background-image: url(back.png);")
 
         
         self.pushButton = QtWidgets.QPushButton(Dialog)
         self.pushButton.setGeometry(QtCore.QRect(60, 90, 93, 91))
         self.pushButton.setIcon(QtGui.QIcon('browser.png'))
-        self.pushButton.setStyleSheet('background-color: blue')
+        self.pushButton.setIconSize(QSize(75,75))
+        self.pushButton.setStyleSheet('background-color: indica')
         self.pushButton.setText("")
         self.pushButton.setObjectName("pushButton")
 
@@ -26,7 +28,8 @@ class Ui_Dialog(object):
         self.pushButton_2 = QtWidgets.QPushButton(Dialog)
         self.pushButton_2.setGeometry(QtCore.QRect(280, 90, 93, 91))
         self.pushButton_2.setIcon(QtGui.QIcon('phonebook.png'))
-        self.pushButton_2.setStyleSheet('background-color: blue')
+        self.pushButton_2.setIconSize(QSize(75, 75))
+        self.pushButton_2.setStyleSheet('background-color: indica')
         self.pushButton_2.setText("")
         self.pushButton_2.setObjectName("pushButton_2")
 
@@ -34,7 +37,8 @@ class Ui_Dialog(object):
         self.pushButton_3 = QtWidgets.QPushButton(Dialog)
         self.pushButton_3.setGeometry(QtCore.QRect(280, 290, 93, 91))
         self.pushButton_3.setIcon(QtGui.QIcon('calender.png'))
-        self.pushButton_3.setStyleSheet('background-color: blue')
+        self.pushButton_3.setIconSize(QSize(75, 75))
+        self.pushButton_3.setStyleSheet('background-color: indica')
         self.pushButton_3.setText("")
         self.pushButton_3.setObjectName("pushButton_3")
 
@@ -42,7 +46,8 @@ class Ui_Dialog(object):
         self.pushButton_4 = QtWidgets.QPushButton(Dialog)
         self.pushButton_4.setGeometry(QtCore.QRect(60, 290, 93, 91))
         self.pushButton_4.setIcon(QtGui.QIcon('calculator.png'))
-        self.pushButton_4.setStyleSheet('background-color: blue')
+        self.pushButton_4.setIconSize(QSize(75, 75))
+        self.pushButton_4.setStyleSheet('background-color: indica')
         self.pushButton_4.setText("")
         self.pushButton_4.setObjectName("pushButton_4")
 
@@ -50,7 +55,8 @@ class Ui_Dialog(object):
         self.pushButton_5 = QtWidgets.QPushButton(Dialog)
         self.pushButton_5.setGeometry(QtCore.QRect(280, 500, 93, 91))
         self.pushButton_5.setIcon(QtGui.QIcon('cloud.png'))
-        self.pushButton_5.setStyleSheet('background-color: blue')
+        self.pushButton_5.setIconSize(QSize(75, 75))
+        self.pushButton_5.setStyleSheet('background-color: indica')
         self.pushButton_5.setText("")
         self.pushButton_5.setObjectName("pushButton_5")
 
@@ -58,7 +64,8 @@ class Ui_Dialog(object):
         self.pushButton_6 = QtWidgets.QPushButton(Dialog)
         self.pushButton_6.setGeometry(QtCore.QRect(60, 500, 93, 91))
         self.pushButton_6.setIcon(QtGui.QIcon('notepad.png'))
-        self.pushButton_6.setStyleSheet('background-color: blue')
+        self.pushButton_6.setIconSize(QSize(75, 75))
+        self.pushButton_6.setStyleSheet('background-color: indica')
         self.pushButton_6.setText("")
         self.pushButton_6.setObjectName("pushButton_6")
 
