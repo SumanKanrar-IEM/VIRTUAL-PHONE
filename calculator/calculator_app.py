86ccdfc16bfca55646ab404ebf7f9db85cbd2090
SumanKanrar-IEM <SumanKanrar-IEM@users.noreply.github.com>
2017-10-11 23:05:07 +0530

    small changes made to python file of calculator

--- calculator/calculator_app.py
+++ calculator/calculator_app.py
@@ -1,8 +1,8 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
-import calculator_vfx
+import calculator_ui
 
-class Calculator_class(calculator_vfx.Ui_MainWindow, QtWidgets.QMainWindow):
+class Calculator_class(calculator_ui.Ui_MainWindow, QtWidgets.QMainWindow):
 
     def __init__(self):
         super(Calculator_class, self).__init__()
