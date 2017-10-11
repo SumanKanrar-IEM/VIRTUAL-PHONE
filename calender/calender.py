2be2d7a7292e748228170dc5043650fc4ce7f13d
SumanKanrar-IEM <SumanKanrar-IEM@users.noreply.github.com>
2017-10-12 02:00:19 +0530

    Minor changes made to add files and categorized for better work

--- calender/calender.py
+++ calender/calender.py
@@ -35,6 +35,8 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Calender"))
+        Form.setWindowIcon(QtGui.QIcon('calender.png'))
+
         self.calendarWidget.setWhatsThis(_translate("Form", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
